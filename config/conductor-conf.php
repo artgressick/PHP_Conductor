528c41b3e1fa2c349662eefeb9c96bb96e21e361
artgressick <artgressick@gmail.com>
2020-01-31 20:43:19 -0500

    first commit

---
+++ config/conductor-conf.php
@@ -0,0 +1,17 @@
+<?
+// This file contains the information needed for the application to connect to its MySQL
+//  data store.  Do not delete this file unless you know what you are doing!
+
+$host = '192.168.10.2';
+$user = '';
+$pass = '';
+$db   = 'conductor';
+
+$PROJECT_NAME = 'Apple Staffing';
+$PROJECT_EMAIL = 'applestaffing@apple.com';
+$PROJECT_ADDRESS = '';
+
+//define('BUG_REPORT_ADDRESS', 'bugs@techitsolutions.com');
+define('DEBUG_CONSOLE', '1');
+
+?>
